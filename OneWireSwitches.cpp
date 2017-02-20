ccc084edb3c5fcfe51b518d5393ee47273156cb6
Fermín Olaiz <ferminolaiz@gmail.com>
2017-02-19 21:48:01 -0300

    Callback support added

--- OneWireSwitches.cpp
+++ OneWireSwitches.cpp
@@ -1,5 +1,31 @@
 #include "OneWireSwitches.h"
 
+	#ifdef OWS_ENABLE_CALLBACKS
+		void OneWireSwitches::setCallbacks(uint8_t Amount, OWSCallback* Cs)
+		{
+			CallbackAmount = Amount;
+			Callbacks = Cs;
+		}
+
+		void OneWireSwitches::readAll()
+		{
+			// Use readPulse()
+			for(uint8_t i = 0; i < CallbackAmount; i++)
+			{
+				uint16_t PulseLength;
+
+				while(readKey(Callbacks[i].R1Index) && PulseLength < Callbacks[i].MaxPulseLength)
+				{
+					PulseLength++;
+					delay(1);
+				}
+
+				if(PulseLength >= Callbacks[i].MinPulseLength)
+					Callbacks[i].Callback();
+			}
+		}
+	#endif
+	
 	// Resistor's tolerance in percent divided by 100. The user previously multiplied it by 10
 	#define OWS_RESISTOR_TOLERANCE Tolerance / 1000.0f
 
@@ -26,5 +52,22 @@
 
 		return false;
 	}
+
+	// Reads a pulse from a button and returns the time since the press
+	// Returns 0 if the button is not pressed
+	uint16_t OneWireSwitches::readPulse(uint8_t Key, uint16_t Timeout = 0)
+	{
+		// Use timeout instead of
+		uint16_t Time = 0;
+
+		while(readKey(Key) == HIGH && (Timeout == 0 || Time < Timeout))
+		{
+			Time++;
+			delay(1);
+		}
+
+		return Time;
+	}
+
 	// Add copyright and license info. 
 	// Add examples and keywords.txt
--- OneWireSwitches.h
+++ OneWireSwitches.h
@@ -5,8 +5,20 @@
 		#include "Arduino.h"
 	#else
 		#include "WProgram.h"
-		#include "pins_arduino.h"
-		#include "WConstants.h"
+	#endif
+
+	//#define OWS_ENABLE_CALLBACKS
+
+	#ifdef OWS_ENABLE_CALLBACKS
+		typedef void (*ows_callback)();
+
+		struct OWSCallback
+		{
+			uint8_t R1Index;
+			uint16_t MinPulseLength;
+			uint16_t MaxPulseLength;
+			ows_callback Callback;
+		};
 	#endif
 
 	class OneWireSwitches
@@ -23,11 +35,24 @@
 			// Then, a 5% tolerance will be 50, and a 1% tolerance will be 10. 
 			uint8_t Tolerance;
 
+			#ifdef OWS_ENABLE_CALLBACKS
+				uint8_t CallbackAmount;
+
+				OWSCallback* Callbacks;
+			#endif 
 		public:
 			OneWireSwitches(uint8_t InputPin, uint8_t KeyAmount, uint32_t* R1Array, uint32_t R2, uint8_t Tolerance = 50)
 			: InputPin(InputPin), KeyAmount(KeyAmount), R1Array(R1Array), R2(R2), Tolerance(Tolerance)
 			{}
 
+			#ifdef OWS_ENABLE_CALLBACKS
+				void setCallbacks(uint8_t Amount, OWSCallback* Cs);
+
+				void readAll();
+			#endif
+
 			bool readKey(uint8_t Key);
+
+			uint16_t readPulse(uint8_t Key, uint16_t Timeout /* = 0*/);
 	};
 #endif
