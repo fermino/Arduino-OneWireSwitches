73311cada972cd295fcd709f962901ccddd00f25
Fermín Olaiz <ferminolaiz@gmail.com>
2017-03-02 17:18:54 -0300

    Some optimizations

--- OneWireSwitches.h
+++ OneWireSwitches.h
@@ -21,11 +21,11 @@
 		};
 	#endif*/
 
-	template <uint8_t KEY_ARRAY_SIZE>
+	template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
 	class OneWireSwitches
 	{
 		private:
-			uint8_t InputPin;
+			uint8_t InputPin = INPUT_PIN;
 
 			uint8_t KeyArraySize = KEY_ARRAY_SIZE;
 			uint16_t ExpectedValues[KEY_ARRAY_SIZE];
@@ -38,7 +38,7 @@
 				OWSCallback* Callbacks;
 			#endif*/
 		public:
-			OneWireSwitches(uint8_t InputPin, uint32_t* R1, uint32_t R2, uint8_t ReadTolerance/* = 25*/);
+			OneWireSwitches(uint32_t* R1, uint32_t R2, uint8_t ReadTolerance = 25);
 
 			/*#ifdef OWS_ENABLE_CALLBACKS
 				void setCallbacks(uint8_t Amount, OWSCallback* Cs);
@@ -48,25 +48,31 @@
 
 			bool readKey(uint8_t Key);
 
-			uint16_t readKeyPulse(uint8_t Key, uint16_t Timeout /* = 0*/);
+			uint16_t readKeyPulse(uint8_t Key, uint16_t Timeout = 0);
 	};
 
-	template <uint8_t KEY_ARRAY_SIZE>
-	OneWireSwitches<KEY_ARRAY_SIZE>::OneWireSwitches(uint8_t InputPin, uint32_t* R1, uint32_t R2, uint8_t ReadTolerance = 25)
+	template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
+	OneWireSwitches<KEY_ARRAY_SIZE, INPUT_PIN>::OneWireSwitches(uint32_t* R1, uint32_t R2, uint8_t ReadTolerance /* = 25 */)
 	{
-		this->InputPin = InputPin;
 		this->ReadTolerance = ReadTolerance * 2;
 
 		for(uint8_t i = 0; i < KeyArraySize; i++)
 			ExpectedValues[i] = round(((float) R2 / (float) (R2 + R1[i])) * 1023) - ReadTolerance;
 	}
 
-	template <uint8_t KEY_ARRAY_SIZE>
-	bool OneWireSwitches<KEY_ARRAY_SIZE>::readKey(uint8_t KeyIndex)
+	template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
+	bool OneWireSwitches<KEY_ARRAY_SIZE, INPUT_PIN>::readKey(uint8_t KeyIndex)
 	{
 		if(KeyIndex < KeyArraySize)
 		{
 			uint16_t Reading = analogRead(InputPin);
+
+			/*Serial.print("i = ");
+			Serial.print(ExpectedValues[KeyIndex]);
+			Serial.print("\t");
+			Serial.print(Reading);
+			Serial.print("\t");
+			Serial.println(ExpectedValues[KeyIndex] + ReadTolerance);*/
 
 			if(Reading < ExpectedValues[KeyIndex] || Reading > (ExpectedValues[KeyIndex] + ReadTolerance))
 				return false;
@@ -77,8 +83,8 @@
 		return false;
 	}
 
-	template <uint8_t KEY_ARRAY_SIZE>
-	uint16_t OneWireSwitches<KEY_ARRAY_SIZE>::readKeyPulse(uint8_t KeyIndex, uint16_t Timeout = 0)
+	template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
+	uint16_t OneWireSwitches<KEY_ARRAY_SIZE, INPUT_PIN>::readKeyPulse(uint8_t KeyIndex, uint16_t Timeout /* = 0 */)
 	{
 		uint16_t Time = 0;
 
