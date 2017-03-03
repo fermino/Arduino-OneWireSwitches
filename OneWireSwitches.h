1623de048cb99a02653d3eae1a8088b2d38187d2
Fermín Olaiz <ferminolaiz@gmail.com>
2017-03-02 23:59:32 -0300

    Callback support added (some tests left)

--- OneWireSwitches.h
+++ OneWireSwitches.h
@@ -9,17 +9,17 @@
 
 	//#define OWS_ENABLE_CALLBACKS
 
-	/*#ifdef OWS_ENABLE_CALLBACKS
+	#ifdef OWS_ENABLE_CALLBACKS
 		typedef void (*ows_callback)();
 
 		struct OWSCallback
 		{
-			uint8_t R1Index;
+			uint8_t KeyIndex;
 			uint16_t MinPulseLength;
 			uint16_t MaxPulseLength;
 			ows_callback Callback;
 		};
-	#endif*/
+	#endif
 
 	template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
 	class OneWireSwitches
@@ -32,22 +32,21 @@
 
 			uint8_t ReadTolerance;
 
-			/*#ifdef OWS_ENABLE_CALLBACKS
-				uint8_t CallbackAmount;
-
+			#ifdef OWS_ENABLE_CALLBACKS
+				uint8_t CallbackArraySize = 0;
 				OWSCallback* Callbacks;
-			#endif*/
+
+				int16_t LastReading = -1;
+			#endif
 		public:
 			OneWireSwitches(uint32_t* R1, uint32_t R2, uint8_t ReadTolerance = 25);
 
-			/*#ifdef OWS_ENABLE_CALLBACKS
+			#ifdef OWS_ENABLE_CALLBACKS
 				void setCallbacks(uint8_t Amount, OWSCallback* Cs);
-
-				void readAll();
-			#endif*/
+				void readKeys();
+			#endif
 
 			bool readKey(uint8_t Key);
-
 			uint16_t readKeyPulse(uint8_t Key, uint16_t Timeout = 0);
 	};
 
@@ -60,12 +59,46 @@
 			ExpectedValues[i] = round(((float) R2 / (float) (R2 + R1[i])) * 1023) - ReadTolerance;
 	}
 
+	#ifdef OWS_ENABLE_CALLBACKS
+		template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
+		void OneWireSwitches<KEY_ARRAY_SIZE, INPUT_PIN>::setCallbacks(uint8_t CS, OWSCallback* C)
+		{
+			CallbackArraySize = CS;
+			Callbacks = C;
+		}
+
+		template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
+		void OneWireSwitches<KEY_ARRAY_SIZE, INPUT_PIN>::readKeys()
+		{
+			// Use the same reading for all buttons
+			LastReading = analogRead(InputPin);
+
+			for(uint8_t i = 0; i < CallbackArraySize; i++)
+			{
+				if(Callbacks[i].Callback != NULL)
+				{
+					uint16_t PulseLength = readKeyPulse(Callbacks[i].KeyIndex, Callbacks[i].MaxPulseLength);
+
+					if(PulseLength >= Callbacks[i].MinPulseLength)
+						Callbacks[i].Callback();
+				}
+			}
+
+			// Clear the flag
+			LastReading = -1;
+		}
+	#endif
+
 	template <uint8_t KEY_ARRAY_SIZE, uint8_t INPUT_PIN>
 	bool OneWireSwitches<KEY_ARRAY_SIZE, INPUT_PIN>::readKey(uint8_t KeyIndex)
 	{
 		if(KeyIndex < KeyArraySize)
 		{
-			uint16_t Reading = analogRead(InputPin);
+			#ifdef OWS_ENABLE_CALLBACKS
+				uint16_t Reading = LastReading != -1 ? LastReading : analogRead(InputPin);
+			#else
+				uint16_t Reading = analogRead(InputPin);
+			#endif
 
 			/*Serial.print("i = ");
 			Serial.print(ExpectedValues[KeyIndex]);
@@ -88,7 +121,7 @@
 	{
 		uint16_t Time = 0;
 
-		while(readKey(KeyIndex) && (Timeout == 0 || Time < Timeout))
+		while(readKey(KeyIndex) && (Time < Timeout || Timeout == 0))
 		{
 			Time++;
 			delay(1);
